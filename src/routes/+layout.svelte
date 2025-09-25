64c919271b2f7bb31f3e3615542f020332fcee8d
PXNX <loool1912@web.de>
2025-09-25 16:42:36 +0200

    fix height

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,10 @@
 </script>
 
 <div
-	class="100dvw 100dvh flex flex-row items-center justify-center bg-gradient-to-br from-pink-500 via-purple-600 to-teal-400 p-4"
+	class="flex min-h-screen w-screen items-center justify-center bg-gradient-to-br from-pink-500 via-purple-600 to-teal-400 p-4"
 	style="font-family:'HP Simplified'"
 >
-	<div class="100dvh flex w-full flex-col sm:w-3xl">
+	<div class="flex w-full flex-col sm:w-3xl">
 		{@render children()}
 	</div>
 </div>
