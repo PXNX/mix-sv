1818c2053462bf24052a4dcd895eb95d83f47a18
PXNX <loool1912@web.de>
2025-11-24 09:53:10 +0000

    adding meta pages

---
+++ src/routes/(public)/terms-of-service/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>MN Map | Privacy Policy</title>
+</svelte:head>
+
+<div class="mx-auto prose lg:prose-xl">
+	<h1 class="text-lg font-bold">Terms of Service</h1>
+	<p>
+		<strong>Effective Date:</strong> 19 July 2025<br />
+		<strong>Last Updated:</strong> 19 July 2025
+	</p>
+
+	todo
+</div>
